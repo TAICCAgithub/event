31863254abfffe18ac0fec910f083e905764301f
Denny Huang <denny0223@gmail.com>
2020-10-28 00:44:44 +0800

    Add config & build script for TCCF

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.spreadsheetKey = "1_oUt93iHe3p1OPu5sNGfi6ujlkUiusK5f9IE0GZMF00"
+
+config.default_avatar = "https://sitcon.org/2018/static/img/staffs/stone.png"
+config.avatar_base_url = "https://sitcon.org/2018/static/img/speaker/"
+
+module.exports = config
