b6a60d58765afc023e0b44c8ee393a5f67b371ff
Denny Huang <denny0223@gmail.com>
2020-11-03 22:19:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.spreadsheetKey = "1_oUt93iHe3p1OPu5sNGfi6ujlkUiusK5f9IE0GZMF00"
 
-config.default_avatar = "https://sitcon.org/2018/static/img/staffs/stone.png"
-config.avatar_base_url = "https://sitcon.org/2018/static/img/speaker/"
+config.default_avatar = "https://taiccagithub.github.io/img/tccf.png"
+config.avatar_base_url = "https://tccf.taicca.tw/images/"
 
 module.exports = config
